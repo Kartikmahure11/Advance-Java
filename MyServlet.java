9fc38d31cc643ea4f9ca76aef5a7a7ef9df3a8fd
Kartik Yuvraj Mahure <131333372+Kartikmahure11@users.noreply.github.com>
2024-02-08 13:27:55 +0530

    Adv_Assignment_1.2

--- MyServlet.java
+++ MyServlet.java
@@ -1,5 +1,3 @@
-
-
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -12,28 +10,30 @@ import javax.servlet.http.HttpServletResponse;
  
 @WebServlet("/MyServlet")
 public class MyServlet extends HttpServlet {
-	     
+ 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
-		  response.setContentType("text/html");
-		  PrintWriter out=response.getWriter();
-		  String id = request.getParameter("realname");
-		  String password = request.getParameter("mypassword");
-		  out.print("Username:"+id+"<br>Password:"+password); 
-	}
+		double principal = Double.parseDouble(request.getParameter("principal"));
+        double rate = Double.parseDouble(request.getParameter("rate"));
+        double time = Double.parseDouble(request.getParameter("time"));
 
-		public void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException 
-		{
-			response.setContentType("text/html");
-			PrintWriter out=response.getWriter();
-			
-		    String id = request.getParameter("realname");
-		    String password = request.getParameter("mypassword");
-		    
-		    out.print("Username:"+id+"<br>Password:"+password); 
-	}
+        double simpleInterest = (principal * rate * time) / 100;
 
-	 
-	 
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><head><title>Simple Interest Calculator Result</title></head><body>");
+        out.println("<h2>Simple Interest Calculator Result</h2>");
+        out.println("<p>Principal: $" + principal + "</p>");
+        out.println("<p>Rate: " + rate + "%</p>");
+        out.println("<p>Time (years): " + time + "</p>");
+        out.println("<p>Simple Interest: $" + simpleInterest + "</p>");
+        out.println("</body></html>");
+	}
+ 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		 
+		doGet(request, response);
+	}
 
 }
